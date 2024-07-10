34f3c9d0a13485a0ea25a04750489216e22b5797
O-orion <lucas.lvtorres@gmail.com>
2024-07-10 17:52:09 -0300

    componente sobre mim

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CapaHomeComponent } from '../components/capa-home/capa-home.component';
+import { SobreMimComponent } from '../components/sobre-mim/sobre-mim.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    CapaHomeComponent
+    CapaHomeComponent,
+    SobreMimComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
