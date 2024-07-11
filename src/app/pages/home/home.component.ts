e10dcb965810e3fecbb96eb1f9a1541a14d9b5cc
O-orion <lucas.lvtorres@gmail.com>
2024-07-11 14:43:04 -0300

    meus projetos component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CapaHomeComponent } from '../components/capa-home/capa-home.component';
 import { SobreMimComponent } from '../components/sobre-mim/sobre-mim.component';
+import { MeusProjetosComponent } from '../components/meus-projetos/meus-projetos.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CapaHomeComponent,
-    SobreMimComponent
+    SobreMimComponent,
+    MeusProjetosComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
